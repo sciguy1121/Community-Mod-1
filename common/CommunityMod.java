7eb0d8b89c38e19f75e87ac3117ab283541e1057
Sciguy1121 <sciguy1121email@gmail.com>
2013-03-02 10:43:05 -0500

    Added load() method

--- common/CommunityMod.java
+++ common/CommunityMod.java
@@ -1,6 +1,8 @@
 package communityMod.common;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "Community_Mod", name = "Community_Mod", version = "1.0")
@@ -8,4 +10,9 @@ import cpw.mods.fml.common.network.NetworkMod;
 
 public class CommunityMod {
 
+	@Init
+	public void load(FMLInitializationEvent event){
+		
+	}
+	
 }
