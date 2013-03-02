8f852efb17242c8efea20d929824d9ea4dea4da5
Sciguy1121 <sciguy1121email@gmail.com>
2013-03-02 10:50:42 -0500

    Added preInit() for config file

--- common/CommunityMod.java
+++ common/CommunityMod.java
@@ -1,8 +1,14 @@
 package communityMod.common;
 
+import net.minecraftforge.common.Configuration;
+import communityMod.client.ClientProxyCommunityMod;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 @Mod(modid = "Community_Mod", name = "Community_Mod", version = "1.0")
@@ -10,6 +16,18 @@ import cpw.mods.fml.common.network.NetworkMod;
 
 public class CommunityMod {
 
+	@SidedProxy(clientSide = "tutorial.client.ClientProxyTutorial",
+				serverSide = "tutorial.common.CommonProxyTutorial")
+	public static ClientProxyCommunityMod proxy = new ClientProxyCommunityMod();
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event){
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+		
+		config.save();
+	}
+	
 	@Init
 	public void load(FMLInitializationEvent event){
 		
