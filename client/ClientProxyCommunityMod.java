c14a2689b0d35092bff1d230f62c041a49c84596
Sciguy1121 <sciguy1121email@gmail.com>
2013-03-02 10:32:05 -0500

    Edits

--- client/ClientProxyCommunityMod.java
+++ client/ClientProxyCommunityMod.java
@@ -1,7 +1,14 @@
 package communityMod.client;
 
+import net.minecraftforge.client.MinecraftForgeClient;
 import communityMod.common.CommonProxyCommunityMod;
+import communityMod.textures.TextureHandler;
 
 public class ClientProxyCommunityMod extends CommonProxyCommunityMod{
+	
+	@Override
+	public void registerRenders(){
+		MinecraftForgeClient.preloadTexture(TextureHandler.BLOCK_TEXTURE_A);
+	}
 	
 }
--- textures/TextureHandler.java
+++ textures/TextureHandler.java
@@ -2,4 +2,6 @@ package communityMod.textures;
 
 public class TextureHandler {
 
+	public static final String BLOCK_TEXTURE_A = "";
+	
 }
