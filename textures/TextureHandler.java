d846e46b6d703e88f4302490fa683c773b894bc0
Sciguy1121 <sciguy1121email@gmail.com>
2013-03-02 10:35:11 -0500

    Added Block Textures File

--- textures/TextureHandler.java
+++ textures/TextureHandler.java
@@ -2,6 +2,6 @@ package communityMod.textures;
 
 public class TextureHandler {
 
-	public static final String BLOCK_TEXTURE_A = "";
+	public static final String BLOCK_TEXTURE_A = "Block_Textures_A";
 	
 }
