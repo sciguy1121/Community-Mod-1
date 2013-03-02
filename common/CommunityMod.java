0240338e01ef18b0e875adaa938bac27eb6f2d9b
Sciguy1121 <sciguy1121email@gmail.com>
2013-03-02 10:19:43 -0500

    Changed name

--- common/CommunityMod.java
+++ common/CommunityMod.java
@@ -6,6 +6,6 @@ import cpw.mods.fml.common.network.NetworkMod;
 @Mod(modid = "Community_Mod", name = "Community_Mod", version = "1.0")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 
-public class ModCommunityMod {
+public class CommunityMod {
 
 }
