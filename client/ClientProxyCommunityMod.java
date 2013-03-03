269f274f391d01ff2aabc44cf72992f3530f3987
Sciguy1121 <sciguy1121email@gmail.com>
2013-03-03 11:22:42 -0500

    New Textures

--- client/ClientProxyCommunityMod.java
+++ client/ClientProxyCommunityMod.java
@@ -9,6 +9,7 @@ public class ClientProxyCommunityMod extends CommonProxyCommunityMod{
 	@Override
 	public void registerRenders(){
 		MinecraftForgeClient.preloadTexture(TextureHandler.BLOCK_TEXTURE_A);
+		MinecraftForgeClient.preloadTexture(TextureHandler.ITEM_TEXTURE_A);
 	}
 	
 }
--- textures/TextureHandler.java
+++ textures/TextureHandler.java
@@ -3,5 +3,6 @@ package communityMod.textures;
 public class TextureHandler {
 
 	public static final String BLOCK_TEXTURE_A = "Block_Textures_A";
+	public static final String ITEM_TEXTURE_A = "Item_Textures_A";
 	
 }
