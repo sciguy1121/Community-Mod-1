e04d45f5b9e69cd9ca94eeacda106992b18557a2
smigolpol <smigolpol@gmail.com>
2013-03-06 21:07:18 +0100

    Progress on lava furnace. It smelts!

--- common/gui/GuiLavaFurnace.java
+++ common/gui/GuiLavaFurnace.java
@@ -31,6 +31,18 @@ public class GuiLavaFurnace extends GuiContainer
 		int x = (this.width - this.xSize) / 2;
 		int y = (this.height - this.ySize) / 2;
 		this.drawTexturedModalRect(x, y, 0, 0, this.xSize, this.ySize);
+		
+//		int progress;          Progress and heat indicators don't work yet.
+//		int heat;
+//		
+//		if(true)
+//		{
+//			heat = entity.heat / 12 * 200;
+//			this.drawTexturedModalRect(x + 80, y + 10 + 12 - heat, 235, 12 - heat, 14, heat + 2);
+//		}
+//		
+//		progress = entity.getBurnTimeRemainingScaled(12);
+//		this.drawTexturedModalRect(x + 82, y + 23, 176, 14, progress + 1, 16);
 	}
 	
 	@Override
