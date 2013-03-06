346603c5b2b483c3e54183326644a831ac7288e9
Sciguy1121 <sciguy1121email@gmail.com>
2013-03-06 17:17:26 -0500

    Formatting Fix

--- common/blocks/BlockLavaFurnace.java
+++ common/blocks/BlockLavaFurnace.java
@@ -36,60 +36,60 @@ public class BlockLavaFurnace extends BlockContainer
 		return new TileEntityLavaFurnace(!active);
 	}
 	
-	  @Override
-	  public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int i, float f, float g, float t){
-		  TileEntityLavaFurnace tile_entity = (TileEntityLavaFurnace)world.getBlockTileEntity(x, y, z);
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int i, float f, float g, float t){
+		TileEntityLavaFurnace tile_entity = (TileEntityLavaFurnace)world.getBlockTileEntity(x, y, z);
 	   
-		  if(tile_entity == null || player.isSneaking())
-		  {
-			  return false;
-		  }
+		if(tile_entity == null || player.isSneaking())
+		{
+			return false;
+		}
 	   
-		  player.openGui(CommunityMod.instance, 0, world, x, y, z);
-		  return true;
-	  }
+		player.openGui(CommunityMod.instance, 0, world, x, y, z);
+		return true;
+	}
 	   
-	  @Override
-	  public void breakBlock(World world, int x, int y, int z, int i, int j){
-		  dropItems(world, x, y, z);
-		  super.breakBlock(world, x, y, z, i, j);
-	  }
+	@Override
+	public void breakBlock(World world, int x, int y, int z, int i, int j){
+		dropItems(world, x, y, z);
+		super.breakBlock(world, x, y, z, i, j);
+	}
 	   
-	  private void dropItems(World world, int x, int y, int z){
-		  Random rand = new Random();
+	private void dropItems(World world, int x, int y, int z){
+		Random rand = new Random();
 		  
-		  TileEntity tile_entity = world.getBlockTileEntity(x, y, z);
+		TileEntity tile_entity = world.getBlockTileEntity(x, y, z);
 		  
-		  if(!(tile_entity instanceof IInventory)){
-			  return;
-		  }
+		if(!(tile_entity instanceof IInventory)){
+			return;
+		}
 	   
-		  IInventory inventory = (IInventory) tile_entity;
+		IInventory inventory = (IInventory) tile_entity;
 	   
-		  for(int i = 0; i < inventory.getSizeInventory(); i++){
-			  ItemStack item = inventory.getStackInSlot(i);
+		for(int i = 0; i < inventory.getSizeInventory(); i++){
+			ItemStack item = inventory.getStackInSlot(i);
 	                   
-			  if(item != null && item.stackSize > 0){
-				  float rx = rand.nextFloat() * 0.6F + 0.1F;
-				  float ry = rand.nextFloat() * 0.6F + 0.1F;
-				  float rz = rand.nextFloat() * 0.6F + 0.1F;
+			if(item != null && item.stackSize > 0){
+				float rx = rand.nextFloat() * 0.6F + 0.1F;
+				float ry = rand.nextFloat() * 0.6F + 0.1F;
+				float rz = rand.nextFloat() * 0.6F + 0.1F;
 	                   
-				  EntityItem entity_item = new EntityItem(world, x + rx, y + ry, z + rz, new ItemStack(item.itemID, item.stackSize, item.getItemDamage()));
+				EntityItem entity_item = new EntityItem(world, x + rx, y + ry, z + rz, new ItemStack(item.itemID, item.stackSize, item.getItemDamage()));
 	                   
-				  if(item.hasTagCompound()){
-					  item.setTagCompound((NBTTagCompound) item.getTagCompound().copy());
-				  }
+				if(item.hasTagCompound()){
+					item.setTagCompound((NBTTagCompound) item.getTagCompound().copy());
+				}
 	   
-				  float factor = 0.5F;
+				float factor = 0.5F;
 	                   
-				  entity_item.motionX = rand.nextGaussian() * factor;
-				  entity_item.motionY = rand.nextGaussian() * factor + 0.2F;
-				  entity_item.motionZ = rand.nextGaussian() * factor;
-				  world.spawnEntityInWorld(entity_item);
-				  item.stackSize = 0;
-			  }
-		  }
-	  }
+				entity_item.motionX = rand.nextGaussian() * factor;
+				entity_item.motionY = rand.nextGaussian() * factor + 0.2F;
+				entity_item.motionZ = rand.nextGaussian() * factor;
+				world.spawnEntityInWorld(entity_item);
+				item.stackSize = 0;
+			}
+		}
+	}
 	    
 	  public String getTextureFile()
 	  {
