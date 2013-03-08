0b187ce5bfe9a6caffc53398307841ab1b91ea42
KingCreeperEli <ess0523@gmail.com>
2013-03-08 06:59:51 -0500

    Update CommunityMod.java

--- common/CommunityMod.java
+++ common/CommunityMod.java
@@ -87,7 +87,7 @@ public class CommunityMod {
 		GameRegistry.addSmelting(IDsHelper.tungstenOreID, new ItemStack(ItemsHelper.tungstenIngot, 1), 0.5F);
 		GameRegistry.addSmelting(IDsHelper.arsmiumOreID, new ItemStack(ItemsHelper.arsmiumIngot, 1), 0.5F);
 		GameRegistry.addSmelting(IDsHelper.urdiumOreID, new ItemStack(ItemsHelper.urdiumIngot, 1), 0.5F);
-        GameRegistry.addSmelting(IDsHelper.franciumOreID, new ItemStack(ItemsHelper.franciumIngot, 1), 0.5F);
+        	GameRegistry.addSmelting(IDsHelper.franciumOreID, new ItemStack(ItemsHelper.franciumIngot, 1), 0.5F);
 	}
 
 	private static void craftingRecipes(){
@@ -111,20 +111,19 @@ public class CommunityMod {
 			"FFF", "FFF", "FFF", 'F', ItemsHelper.franciumIngot
 		});
         
-        GameRegistry.addRecipe(new ItemStack(BlocksHelper.concrete, 4), new Object[]{
-        "GSG", "SWS", "GSG", 'G', Block.gravel, 'S', Block.sand, 'W', Item.bucketWater
-       	});
+        	GameRegistry.addRecipe(new ItemStack(BlocksHelper.concrete, 4), new Object[]{
+        	"GSG", "SWS", "GSG", 'G', Block.gravel, 'S', Block.sand, 'W', Item.bucketWater
+       		});
         
-        GameRegistry.addRecipe(new ItemStack(BlocksHelper.concrete, 4), new Object[]{
-        "SGS", "GWG", "SGS", 'G', Block.gravel, 'S', Block.sand, 'W', Item.bucketWater
-        });
+        	GameRegistry.addRecipe(new ItemStack(BlocksHelper.concrete, 4), new Object[]{
+        	"SGS", "GWG", "SGS", 'G', Block.gravel, 'S', Block.sand, 'W', Item.bucketWater
+        	});
         
-        GameRegistry.addRecipe(new ItemStack(BlocksHelper.lavafurnace, 1), new Object[]
-        		{
+       		GameRegistry.addRecipe(new ItemStack(BlocksHelper.lavafurnace, 1), new Object[]{
         			"SSS", "S S", "XXX", 'S', Block.cobblestone, 'X', Item.ingotIron
         		});
         	
-        GameRegistry.addShapelessRecipe(new ItemStack(ItemsHelper.rasberryPie, 1),
-        new ItemStack(Item.sugar), new ItemStack(Item.egg), new ItemStack(ItemsHelper.rasberry));
+        	GameRegistry.addShapelessRecipe(new ItemStack(ItemsHelper.rasberryPie, 1),
+        	new ItemStack(Item.sugar), new ItemStack(Item.egg), new ItemStack(ItemsHelper.rasberry));
 	}
 }
