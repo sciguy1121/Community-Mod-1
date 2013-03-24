90b51e6c9db843029699ee5fc194a9090bc26d1c
KingCreeperEli <ess0523@gmail.com>
2013-03-24 10:46:21 -0400

    Added more attributes to Robot
    (still can't spawn it)

--- common/entities/EntityRobot.java
+++ common/entities/EntityRobot.java
@@ -1,15 +1,54 @@
 package mods.communityMod.common.entities;
 
+<<<<<<< HEAD:common/entities/EntityRobot.java
 import mods.communityMod.common.items.ItemsHelper;
+=======
+import net.minecraft.entity.Entity;
+>>>>>>> Added more attributes to Robot:common/EntityRobot.java
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.monster.EntitySkeleton;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class EntityRobot extends EntityLiving{
     
 	public EntityRobot(World par1World) {
 		super(par1World);
-		this.texture = "/textures/mob/Robot.png";
+		this.texture = "/mob/Robot.png";
 		this.getNavigator().setAvoidsWater(true);
+		this.moveSpeed = 0.23F;
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 16.0F, 0, true));
+	}
+	
+	
+	
+	protected void entityInit(){
+        super.entityInit();
+        this.dataWatcher.addObject(13, new Byte((byte)0));
+    }
+	
+	protected String getLivingSound(){
+        return "none";
+    }
+	
+	protected String getDeathSound(){
+        return "none";
+    }
+	
+	protected String getHurtSound(){
+        return "none";
+    }
+	
+	protected boolean canDespawn(){
+        return false;
+    }
+	
+	protected void fall(float par1){
+		
 	}
 
 	public int getMaxHealth(){
@@ -25,4 +64,14 @@ public class EntityRobot extends EntityLiving{
 		return ItemsHelper.siliconChip.itemID;
 	}
 	
+	public void initCreature()
+    {
+        if (this.worldObj.rand.nextInt(100) == 0)
+        {
+            EntityRobot entityrobot = new EntityRobot(this.worldObj);
+            entityrobot.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, 0.0F);
+            entityrobot.initCreature();
+            this.worldObj.spawnEntityInWorld(entityrobot);
+        }
+    }
 }
