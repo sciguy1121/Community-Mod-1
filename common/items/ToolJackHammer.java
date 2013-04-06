17c279b1f55c102ade5e30c346d1052675f6b45b
Jeremiah Osunjimi <jeremiah9@outlook.com>
2013-04-06 19:18:07 -0400

    Small changes to JackHammer.

--- common/items/ToolJackHammer.java
+++ common/items/ToolJackHammer.java
@@ -5,18 +5,26 @@ import mods.communityMod.common.blocks.BlocksHelper;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
 
 public class ToolJackHammer extends ItemTool {
     private static final Block[] blocksEffectiveAgainst = new Block[]{Block.stone, Block.stoneBrick, BlocksHelper.concrete
     ,Block.blockSteel, Block.anvil, Block.cobblestone};
 
-    protected ToolJackHammer(int par1, int par2, EnumToolMaterial par3EnumToolMaterial, Block[] par4ArrayOfBlock) {
+    private ToolJackHammer(int par1, int par2, EnumToolMaterial par3EnumToolMaterial, Block[] par4ArrayOfBlock) {
         super(par1, par2, par3EnumToolMaterial, blocksEffectiveAgainst);
         this.setCreativeTab(CommunityMod.modTab);
+        this.setMaxStackSize(1);
+        this.setNoRepair();
+    }
+
+    @Override
+    public int getItemEnchantability(){
+        return 0;
     }
 
     public void updateIcons(IconRegister reg){
-        iconIndex = reg.registerIcon("communityMod" + getUnlocalizedName());
+        iconIndex = reg.registerIcon("communityMod:" + getUnlocalizedName());
     }
 }
