b0924c9e62b96bb0c44fe00b93aad0b8cae648a1
DeathSeeker512 <deathseeker152@gmail.com>
2013-07-05 19:30:12 -0500

    fixed the guis

--- resources/assets/communitymod/textures/TextureHandler.java
+++ resources/assets/communitymod/textures/TextureHandler.java
@@ -2,13 +2,13 @@ package assets.communitymod.textures;
 
 public class TextureHandler {
 
-	public static final String BLOCK_TEXTURE_A = "/assets/communityMod/textures/Block_Textures_A.png";
-	public static final String ITEM_TEXTURE_A = "/assets/communityMod/textures/Item_Textures_A.png";
-	public static final String MACHINE_TEXTURE_A = "/assets/communityMod/textures/Machine_Textures_A.png";
-	public static final String LAVAFURNACE_GUI = "/assets/communityMod/textures/gui/LavaFurnaceGUI.png";
-	public static final String ENCHANTER_GUI = "/assets/communityMod/textures/gui/EnchanterGUI.png";
-	public static final String INFUSER_GUI = "/assets/communityMod/textures/gui/InfuserGUI.png";
-	public static final String CONCRETEALLOY_GUI = "/assets/communityMod/textures/gui/ConcreteAlloyGUI.png";
-	public static final String RESEARCHER_GUI = "/assets/communityMod/textures/gui/ResearcherGUI.png";
-	public static final String LOGGER_GUI = "/assets/communityMod/textures/gui/LoggerGUI.png";
+	public static final String BLOCK_TEXTURE_A = "communitymod:textures/Block_Textures_A.png";
+	public static final String ITEM_TEXTURE_A = "communitymod:textures/Item_Textures_A.png";
+	public static final String MACHINE_TEXTURE_A = "communitymod:textures/Machine_Textures_A.png";
+	public static final String LAVAFURNACE_GUI = "communitymod:textures/gui/LavaFurnaceGUI.png";
+	public static final String ENCHANTER_GUI = "communitymod:textures/gui/EnchanterGUI.png";
+	public static final String INFUSER_GUI = "communitymod:textures/gui/InfuserGUI.png";
+	public static final String CONCRETEALLOY_GUI = "communitymod:textures/gui/ConcreteAlloyGUI.png";
+	public static final String RESEARCHER_GUI = "communitymod:textures/gui/ResearcherGUI.png";
+	public static final String LOGGER_GUI = "communitymod:textures/gui/LoggerGUI.png";
 }
